96c6199c3a7ed408eb871fa5db6f60920c29f6f1
Samuel Adewole <samueladewole15@gmail.com>
2019-02-02 13:05:46 +0100

    implemented the delete button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todo from './Todo'
+import AddTodo from './addTodo'
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'Buy milk'},
+      {id: 2, content: 'Pay my bill'}
+    ]
+  }
+
+  deleteTodo = (id) =>{
+    let todos = this.state.todos.filter(todo=>{
+      return todo.id !== id
+    })
+
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo= (todo)=>{
+    todo.id = Math.random()
+
+    const todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todo todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
